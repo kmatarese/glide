5ccec7b8f3649fb0f4795aae013b6c70a2168eb3
Kurt Matarese <kmatarese@gmail.com>
2019-08-08 13:54:33 -0400

    update sphinx conf

--- docs/conf.py
+++ docs/conf.py
@@ -27,6 +27,9 @@ release = '0.1.0'
 
 # -- General configuration ---------------------------------------------------
 
+# Necessary for ReadTheDocs
+master_doc = 'index'
+
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
